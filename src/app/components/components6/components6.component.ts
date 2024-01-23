519f934e7ef9490ed9ca8004f41576a083da3dd9
Mesias02 <146044728+Mesias02@users.noreply.github.com>
2024-01-23 08:46:19 -0500

    commint

---
+++ src/app/components/components6/components6.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-components6',
+  standalone: true,
+  imports: [],
+  templateUrl: './components6.component.html',
+  styleUrl: './components6.component.css'
+})
+export class Components6Component {
+  
+
+}
